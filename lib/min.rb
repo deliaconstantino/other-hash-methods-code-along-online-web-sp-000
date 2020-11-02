a0f1becac55d1018faa5889297d3070087c62829
Delia Constantino <dconstan92@gmail.com>
2020-11-02 17:40:09 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -11,8 +11,8 @@ require 'pry'
 def get_the_min(groceries)
   grocery_items = []
   groceries.each do |category, item_array|
-    item_array.min do |item|
-      grocery_items << item
+    item_array.min do |item_a, item_b|
+      grocery_items << item_a <=> item_b
     end
   end
 
